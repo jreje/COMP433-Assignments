bc3e735e85073acc6d8eec1d799a3877cf26a2c2
Julianne Rejesus <jreje@ad.unc.edu>
2025-10-07 13:02:00 -0400

    UI

---
+++ CW3/app/src/main/java/com/example/cw3/DrawingArea.java
@@ -0,0 +1,27 @@
+package com.example.cw3;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class DrawingArea extends View {
+    public DrawingArea(Context context) {
+        super(context);
+    }
+
+    public DrawingArea(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DrawingArea(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public DrawingArea(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+
+}
