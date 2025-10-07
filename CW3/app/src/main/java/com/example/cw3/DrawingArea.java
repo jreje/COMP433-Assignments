95d4d48ee5a9831b974909a5971f6523ee1e81a4
Julianne Rejesus <jreje@ad.unc.edu>
2025-10-07 13:30:50 -0400

    Drawing Logic

--- CW3/app/src/main/java/com/example/cw3/DrawingArea.java
+++ CW3/app/src/main/java/com/example/cw3/DrawingArea.java
@@ -1,12 +1,27 @@
 package com.example.cw3;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class DrawingArea extends View {
+    private Path drawing = new Path();
+    private Bitmap bmp;
+
     public DrawingArea(Context context) {
         super(context);
     }
@@ -23,5 +38,50 @@ public class DrawingArea extends View {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
 
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint p = new Paint();
+        p.setColor(Color.BLACK);
+        p.setStyle(Paint.Style.STROKE);
+        p.setStrokeWidth(5f);
 
+        canvas.drawPath(drawing, p);
+        invalidate();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX(), y = event.getY();
+        int action = event.getAction();
+        if (action == MotionEvent.ACTION_DOWN) {
+            drawing.moveTo(x, y);
+        }
+        else if (action == MotionEvent.ACTION_MOVE) {
+            drawing.lineTo(x, y);
+        }
+        invalidate();
+        return true;
+    }
+
+    public Bitmap getBitmap() {
+        // Turn canvas drawing to to bitmap
+        bmp = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(bmp);
+        Paint p = new Paint();
+        p.setColor(Color.BLACK);
+        p.setStyle(Paint.Style.STROKE);
+        p.setAntiAlias(true);
+        p.setStrokeWidth(5f);
+        c.drawPath(drawing, p);
+        return bmp;
+    }
+
+    public void clear(){
+        // clear path
+        drawing.reset();
+
+        // Redraw canvas so the path doesn't appear
+        invalidate();
+    }
 }
--- CW3/app/src/main/java/com/example/cw3/MainActivity.java
+++ CW3/app/src/main/java/com/example/cw3/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cw3;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 
@@ -9,7 +10,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+    // DrawingArea variables
+    View drawingAreaView;
+    DrawingArea drawingArea;
+
+    // File Variables
+    File drawingsDirectory;
+    File[] drawingFilesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +35,31 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        drawingAreaView = findViewById(R.id.drawingArea);
+        drawingArea = findViewById(R.id.drawingArea);
     }
 
     public void clear(View view) {
+        drawingArea.clear();
+    }
+
+    public void save(View view) {
+        Bitmap drawingBitmap = drawingArea.getBitmap();
+        try {
+            // Create file
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                    Locale.US).format(new Date());
+            File f = new File(getFilesDir().getAbsolutePath(), "IMG_" + timeStamp + ".png");
+
+            // Open file stream so we can write our image to the file we created
+            FileOutputStream fos = new FileOutputStream(f);
+            // compress the bitmap drawing in order to write the file
+            drawingBitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+
+            // Update files
+            drawingFilesList = drawingsDirectory.listFiles();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
